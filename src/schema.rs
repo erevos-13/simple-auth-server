c8fca5238095831cd90275256a4714629de4e887
orpheasboutsarides <erevos13@gmail.com>
2023-08-16 11:16:19 +0300

    init

---
+++ src/schema.rs
@@ -0,0 +1,22 @@
+// @generated automatically by Diesel CLI.
+
+diesel::table! {
+    invitations (id) {
+        id -> Uuid,
+        email -> Varchar,
+        expires_at -> Timestamp,
+    }
+}
+
+diesel::table! {
+    users (email) {
+        email -> Varchar,
+        hash -> Varchar,
+        created_at -> Timestamp,
+    }
+}
+
+diesel::allow_tables_to_appear_in_same_query!(
+    invitations,
+    users,
+);
